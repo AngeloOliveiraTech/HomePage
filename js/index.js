73e975b0919645611159ba7051de1707e1772277
AngeloOliveiraTech <angelodeoliveira123@gmail.com>
2023-10-09 10:45:59 -0300

    master

---
+++ js/index.js
@@ -0,0 +1,13 @@
+document.getElementById('bootstrap').addEventListener("click", function(){
+     window.location.href = "https://getbootstrap.com/";
+});
+
+document.getElementById('materialize').addEventListener("click", function(){
+     window.location.href = "https://materializecss.com/";
+});
+
+
+
+
+
+
