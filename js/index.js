1915ac6a0a6c6b004bd602ff2b6938658e295204
AngeloOliveiraTech <angelodeoliveira123@gmail.com>
2024-01-11 15:29:37 -0300

    Atualizacao_1_SEM2024

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-document.getElementById('bootstrap').addEventListener("click", function(){
-     window.location.href = "https://getbootstrap.com/";
-});
+var urlAtual = window.location.pathname;
 
-document.getElementById('materialize').addEventListener("click", function(){
-     window.location.href = "https://materializecss.com/";
-});
+if(urlAtual != "/curriculum.html") {
+     document.getElementById('bootstrap').addEventListener("click", function(){
+          window.location.href = "https://getbootstrap.com/";
+     });
+     
+     document.getElementById('materialize').addEventListener("click", function(){
+          window.location.href = "https://materializecss.com/";
+     });
+}
 
+let el = document.getElementById("skills");
+let options = {
+     series: [{
+          data: [5,5,5,4,3,3,2],
+   }],
+     chart: {
+          type: 'bar',
+          height: 200
+   },
+   theme: {
+     palette: 'palette10' 
+   },
+   plotOptions: {
+     bar: {
+       borderRadius: 5,
+       horizontal: true,
+     }
+   },
+   dataLabels: {
+     enabled: false
+   },
+   xaxis: {
+     categories: ['.NET','PHP','CSS','SQL','NodeJS','ReactJS','Python'
+     ],
+   }
+   };
 
-
-
-
+let chart = new ApexCharts(el, options);
+chart.render();
 
